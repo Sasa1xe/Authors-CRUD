80c87e799c8c79d1341464ad6bc47067bc4b6a48
Sasa1xe <mostafa2005mostafa1@gmail.com>
2026-09-15 02:51:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,122 +11,39 @@ app.use((req, res, next) => {
   next();
 });
 
-// app.use((req, res, next) => {
-//   if (req.method == "GET" && req.url == "/users") {
-//     res.json("hello");
-//   }
-//   next();
-// });
-
-app.get("/users", async (req, res) => {
-  // get all users from db
-  const users = await db.getAll("users");
-
-  // send as json
-  res.json({
-    data: users,
-  });
+app.use((req, res, next) => {
+  req.db = db;
+  next();
 });
 
-// users/1
-// users/2
-// users/34567
-// users/90
-app.get("/users/:user_id", async (req, res) => {
-  // get user by id from database
-  const user = await db.getById("users", req.params.user_id);
-
-  //send as json
-  res.json({
-    data: user,
-  });
-});
-
-app.post("/users", async (req, res) => {
-  // get data from req body
-  const userData = req.body;
-
-  // add to database
-  await db.create("users", userData);
-
-  // return response
-  res.status(201).json({
-    message: "user created successfully",
-  });
-});
-
-app.patch("/users/:user_id", async (req, res) => {
-  // get id from params
-  const id = req.params.user_id;
-
-  // check database
-  const user = await db.getById("users", id);
-
-  if (!user) {
-    return res.status(404).json({
-      message: "user not found",
-    });
-  }
-
-  // get data from body
-  const updateData = req.body;
-
-  // update in database
-  await db.update("users", id, updateData);
-
-  const newUser = await db.getById("users", id);
-
-  // return res
-  return res.status(200).json({
-    message: "user updated successfully",
-    data: newUser,
-  });
-});
-
-app.delete("/users/:user_id", async (req, res) => {
-  // get id from params
-  const id = req.params.user_id;
-
-  // check if in database
-  const user = await db.getById("users", id);
-
-  // if present delete -> return res
-  if (user) {
-    await db.delete("users", id);
-    return res.status(204).json({
-      message: "user deleted successfully",
-    });
-  } else {
-    // if not present -> 404
-    return res.status(404).json({
-      message: "user not found",
-    });
-  }
-});
-
-// get all authors
-app.get("/authors", async (req, res) => {
-  const authors = await db.getAll("authors");
-
-  // /authors?search=ahmed
+//Query Search middleware
+app.get("/authors", async (req, req) => {
   const search = req.query.search;
 
-  if (search) {
-    const filteredAuthors = authors.filter((author) =>
-      author.name.toLowerCase().startsWith(search.toLowerCase())
-    );
-
-    return res.json({
-      data: filteredAuthors,
-    });
+  if (!search) {
+    return next();
   }
+
+  const authors = await req.db.getAll("authors");
+
+  const filteredAuthors = authors.filter((author) =>
+    author.name.toLowerCase().startsWith(search.toLowerCase())
+  );
+
+  return res.json({
+    data: filteredAuthors,
+  });
+});
+//get all 
+app.get("/authors", async (req, res) => {
+  const authors = await db.getAll("authors");
 
   return res.json({
     data: authors,
   });
+
+
 });
-
-
 // get one author
 app.get("/authors/:author_id", async (req, res) => {
   const author = await db.getById("authors" , req.params.author_id);
